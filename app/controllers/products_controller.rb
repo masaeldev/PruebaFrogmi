45d667b36bd93ad687ee4ed5a70e1e15ec22441b
DANILO PUENTE <danilo.dspm.1996@gmail.com>
2024-05-01 13:36:24 -0500

    actualizado rest-client y ffi

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,9 @@
+require 'json'
+require 'rest-client'
 class ProductsController < ApplicationController
     def index
-        
+        url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson'
+        response = RestClient.get url
+        result = JSON.parse response.to_str
     end
 end
