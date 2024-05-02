ef1c1ea170a1578f6f703683ac7e499673772727
DANILO PUENTE <danilo.dspm.1996@gmail.com>
2024-05-02 09:15:45 -0500

    ACTUALIZANDO REST-CLIENT

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,24 @@
 require 'json'
 require 'rest-client'
 class ProductsController < ApplicationController
-    def index
+    
+    def index        
         url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson'
-        response = RestClient.get url
-        result = JSON.parse response.to_str
+        #response = RestClient.get url
+        
+        #response = RestClient::Request.new(
+        #    :method => :get,
+        #    :url => url
+        #).execute
+
+        #response = RestClient::Request.execute(method: :get, url: url)
+
+        @result = ""
+        RestClient.get(url){|response, request, resulr|
+            if response.code == 200
+                @result = JSON.parse response.to_str
+            end
+        }
+        return @result
     end
 end
